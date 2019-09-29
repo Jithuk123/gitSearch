ec91af2559e1550bda24863a97216cc7bcbda514
jithu <jithu.jose@experionglobal.com>
2019-09-29 23:20:15 +0530

    Git search app localstorage created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WrapperComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/wrapper/wrapper.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
+@Component({
+  selector: 'app-wrapper',
+  templateUrl: './wrapper.component.html',
+  styleUrls: ['./wrapper.component.css']
+})
+export class WrapperComponent implements OnInit {
+
+  success: boolean;
+
+  constructor(private toastr: ToastrService, private spinner: MatProgressSpinnerModule, private httpClient: HttpClient
+  ) { }
+  private baseUrl = 'https://api.github.com/users/';
+  input: string;
+  value = {};
+
+
+  getDetails() {
+    this.success = false;
+
+    this.httpClient.get(this.baseUrl + this.input + `?access_token=a839e8d6a73d36f991c2e52ac5fb7e389427e511`).subscribe((res) => {
+      this.success = true;
+      this.value = res;
+    },
+
+      () => {
+        this.toastr.error('Invalid Username', '', {
+          timeOut: 3000
+        });
+      });
+    localStorage.setItem('dataSource', this.input);
+    console.log(localStorage.getItem('dataSource'));
+
+  }
+
+  ngOnInit() {
+    this.success =  true;
+
+  }
+
+}
